9a89c80507939d74e882cc7bb4bba385f3106610
evanwu <wushizhi@jianke.com>
2024-12-17 17:35:56 +0800

    merge flask db heads after merging main.

---
+++ api/migrations/versions/2024_12_17_0852-b90b587766cb_merge_heads.py
@@ -0,0 +1,25 @@
+"""merge heads
+
+Revision ID: b90b587766cb
+Revises: cf8f4fc45278, b5c9da605a74, 3b6551091d65
+Create Date: 2024-12-17 08:52:00.510494
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b90b587766cb'
+down_revision = ('cf8f4fc45278', 'b5c9da605a74', '3b6551091d65')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
